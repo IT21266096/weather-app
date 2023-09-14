d61ab03b11eae97345ec44e6100cc92970bc8c3b
Dumindu Lakshan <lakshand969@gmail.com>
2023-09-14 21:58:54 +0530

    updated

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('./api', userRouter);
 
 app.get("/", (req, res) => {
-    res.send("Main page!");
+    res.send("Main page Go!");
   });
 
 // app.use((err, req, res, next) => {
